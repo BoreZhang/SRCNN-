ce3835d1c3727b49c7eb161131978b74e4edf773
BoreZhang <149680474+BoreZhang@users.noreply.github.com>
2024-01-26 11:33:37 +0000

    Update model.py

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ import torch.nn.init as init
 class SRCNN(nn.Module):
     def __init__(self):
         super(SRCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)  # First convolutional layer
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=4)  # First convolutional layer
         self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)  # Second convolutional layer
         self.conv3 = nn.Conv2d(32, 16, kernel_size=3, padding=1)  # Third convolutional layer
-        self.conv4 = nn.Conv2d(16, 1, kernel_size=5, padding=2)  # Fourth convolutional layer
+        self.conv4 = nn.Conv2d(32, num_channels, kernel_size=5, padding=2)  # Fourth convolutional layer
         
     def forward(self, x):
         out = F.relu(self.conv1(x))  # Apply ReLU activation to the output of the first convolutional layer
